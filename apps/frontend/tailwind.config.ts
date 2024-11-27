ed13fbb77b5671bda1e91422340103f2be8dc5bc
Gisele-Nuness <giselepucca2008@hotmail.com>
2024-11-26 22:13:29 -0300

    commit

---
+++ apps/frontend/tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        'violet-dark': {
+          DEFAULT: '#0E001D',
+          foreground: '#FFF',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
